8fbff259dde8e5ec848740e04fd8f5d66778cb75
Riley Hayes <riley@rileyhayes.au>
2024-05-04 13:34:53 +1000

    Implimented threading to improve InfluxDB writes

--- ruby.py
+++ ruby.py
@@ -2,6 +2,7 @@ import can
 import cantools
 import time
 import yaml
+import threading
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
@@ -23,8 +24,13 @@ write_api = client.write_api(write_options=SYNCHRONOUS)
 bus = can.interface.Bus(channel=can_interface, bustype='socketcan', buffer_size=10000)
 start_time = time.time()
 
+def write_to_influx(messages_to_write):
+    """Function to handle database writes in a separate thread."""
+    write_api.write(influxdb_config['bucket'], influxdb_config['org'], messages_to_write)
+
+messages = []  # Initialize the list to store message points before batching to InfluxDB
+
 try:
-    messages = []  # Initialize the list to store message points before batching to InfluxDB
     while True:
         message = bus.recv()
         if message:
@@ -58,8 +64,9 @@ try:
         elapsed_time = current_time - start_time
         message_count = len(messages)
 
-        if elapsed_time > 4 or message_count >= 600:
-            write_api.write(influxdb_config['bucket'], influxdb_config['org'], messages)
+        if elapsed_time > 10 or message_count >= 1000:
+            # Create a separate thread to write data to InfluxDB
+            threading.Thread(target=write_to_influx, args=(messages.copy(),)).start()
             messages.clear()
             start_time = current_time
 
